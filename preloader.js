d7bc0eeb97a84d95cf3b6fcaae365143b3bf53c8
stalinrangel <e.stalinrangel@gmail.com>
2024-06-22 15:22:33 -0400

    Update preloader.js

--- preloader.js
+++ preloader.js
@@ -16,7 +16,7 @@ overlay.style.cssText = `
 
 const svgImage = document.createElement('img');
 svgImage.id = 'svgImage';
-svgImage.src = 'https://stalinrangel.github.io/preloaderJs/imagem.svg';
+svgImage.src = 'https://stalinrangel.github.io/preloaderJs/imagem.gif';
 svgImage.style.cssText = `
     max-width: 100%;
     max-height: 100%;
